5eeea748e2bcb12e7a5f9fcd900ff33f46edbe5f
jeffreyhong12 <118666607+jeffreyhong12@users.noreply.github.com>
2022-11-28 16:24:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -57,7 +57,12 @@ void Introduce() { //Game introduction function
 	system("cls");
 
 	char back_flag = 'N';
-	cout << "[System]=> \n" << endl;         //Game play introduction
+	cout << "Game Play Introduction: This game is a turn-based matchmaking game. There are a total of three Pokemon Gyms (three game levels). Players need to choose two Pokemon as their pets in each Gym, and then use their own two Pokémons to battle with the enemy's Pokemons. Win all three Gyms to achieve victory." << endl;         //Game play introduction
+	cout <<" "<<endl;
+	cout <<"Detailed Game Description: Before players do battle, they need to choose two different Pokemon from the pool of Pokemon and put them into their backpacks. The enemy will also have two different Pokemon. Only one Pokemon can be on the field for the player and the enemy to fight at the same time. If this Pokemon on the field dies, the next one will be replaced automatically."<<endl;
+	cout <<"Each Pokemon has two types of attacks, normal attacks and skill attacks. Normal attacks cause more damage, skill attacks cause less damage but will have special effects. There are currently two special effects, burning and stunned. If the enemy is hit by the burning effect, he will take some damage at the beginning of the next turn. If the enemy is stunned, there is a certain chance that he will be unable to act in the next turn."<<endl;
+	cout <<" "<<endl;
+	cout <<"For more details, please see the readme file "<<endl;
 	Sleep(500);
 	cout << "\n[System]=> " << "GO BACK ? (Y/N)" << endl; //Exit Options
 	while (back_flag != 'Y') { //keep showing the game introduction if not choose y
